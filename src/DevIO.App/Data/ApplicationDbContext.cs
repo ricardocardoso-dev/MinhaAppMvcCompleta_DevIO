884d1315371a7ff45bd20b8d46316634cfd4818c
Ricardo Cardoso <Ricardo.Cardoso@live.com>
2022-02-18 03:15:01 -0300

    M11A08-Scaffolding + Correção de referências de NuGet Packages

--- src/DevIO.App/Data/ApplicationDbContext.cs
+++ src/DevIO.App/Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using DevIO.App.ViewModels;
 
 namespace DevIO.App.Data
 {
--- src/DevIO.App/Startup.cs
+++ src/DevIO.App/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
+
 namespace DevIO.App
 {
     public class Startup
